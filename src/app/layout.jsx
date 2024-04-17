03b35ef8fb7b74f292bde67e0a2aa1dd7c7e5fbe
Nikolay Vasilev <nikolay.vasilev@veraio.tech>
2024-04-17 13:18:23 +0300

    init project, config files

---
+++ src/app/layout.jsx
@@ -0,0 +1,20 @@
+import "@/styles/global.css";
+
+export const metadata = {
+  title: "Cozy Casa Property | Find The Perfect Home",
+  description:
+    "Find the perfect home for you and your family. We have a wide range of properties to choose from. Contact us today to find your dream home.",
+  keywords: "property, home, real estate, cozy casa, cozy casa property",
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
