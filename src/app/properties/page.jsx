a82b31228585ddc6e3fa98f1a81a5928fc1d287a
Nikolay Vasilev <nyvasilev11@gmail.com>
2024-05-03 22:04:44 +0300

    add properties page

--- src/app/properties/page.jsx
+++ src/app/properties/page.jsx
@@ -1,13 +1,20 @@
-import React from "react";
-import Link from "next/link";
+import PropertyCard from 'components/PropertyCard'
+import properties from '/properties.json'
 
-const PropertiesPage = () => {
-  return (
-    <div>
-      <h1 className="text-3xl">Properties</h1>
-      <Link href="/">Go Home</Link>
+const PropertiesPage = () => (
+  <section className="px-4 py-6">
+    <div className="container-xl lg:container m-auto px-4 py-6">
+      {!properties.length ? (
+        <p className="text-center text-gray-500">No properties found.</p>
+      ) : (
+        <div className="grid gri-cols-1 md:grid-cols-3 gap-6">
+          {properties.map((property) => (
+            <PropertyCard key={property._id} property={property} />
+          ))}
+        </div>
+      )}
     </div>
-  );
-};
+  </section>
+)
 
-export default PropertiesPage;
+export default PropertiesPage
