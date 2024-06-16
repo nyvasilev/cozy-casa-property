8b94f806f707d1b98b7116bee172a80b2c22b20c
Nikolay Vasilev <nyvasilev11@gmail.com>
2024-06-16 16:49:04 +0300

    add a spinner

--- src/app/properties/[id]/page.jsx
+++ src/app/properties/[id]/page.jsx
@@ -9,6 +9,7 @@ import PropertyDetails from '@/src/app/components/PropertyDetails'
 import PropertyContactForm from '@/src/app/components/PropertyContactForm'
 import BookmarkProperty from '@/src/app/components/BookmarkProperty'
 import ShareButtons from '@/src/app/components/ShareButtons'
+import Spinner from '@/src/app/components/Spinner'
 
 const PropertiesPage = () => {
   const { id } = useParams()
@@ -42,34 +43,36 @@ const PropertiesPage = () => {
     return <h1 className="text-center text-2xl font-bold ml-10">Property not found</h1>
 
   return (
-    !loading &&
-    property && (
-      <>
-        <PropertyHeaderImage image={property.images[0]} />
-        <section>
-          <div className="container m-auto py-6 px-6">
-            <Link
-              href="/properties"
-              className="text-blue-500 hover:text-blue-600 flex-items-center">
-              <FaArrowLeft className="inline mr-2" /> Back to properties
-            </Link>
-          </div>
-        </section>
+    <>
+      {loading && <Spinner loading={loading} />}
+      {!loading && property && (
+        <>
+          <PropertyHeaderImage image={property.images[0]} />
+          <section>
+            <div className="container m-auto py-6 px-6">
+              <Link
+                href="/properties"
+                className="text-blue-500 hover:text-blue-600 flex-items-center">
+                <FaArrowLeft className="inline mr-2" /> Back to properties
+              </Link>
+            </div>
+          </section>
 
-        <section className="bg-blue-50">
-          <div className="container m-auto py-10 px-6">
-            <div className="grid grid-cols-1 md:grid-cols-70/30 gap-6">
-              <PropertyDetails property={property} />
-              <div>
-                <BookmarkProperty />
-                <ShareButtons />
-                <PropertyContactForm />
+          <section className="bg-blue-50">
+            <div className="container m-auto py-10 px-6">
+              <div className="grid grid-cols-1 md:grid-cols-70/30 gap-6">
+                <PropertyDetails property={property} />
+                <div>
+                  <BookmarkProperty />
+                  <ShareButtons />
+                  <PropertyContactForm />
+                </div>
               </div>
             </div>
-          </div>
-        </section>
-      </>
-    )
+          </section>
+        </>
+      )}
+    </>
   )
 }
 
