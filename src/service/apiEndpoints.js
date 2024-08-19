0cf413930426548e717c790191c97334499753bd
Nikolay Vasilev <nyvasilev11@gmail.com>
2024-08-20 00:39:54 +0300

    data fetching issue

--- src/service/apiEndpoints.js
+++ src/service/apiEndpoints.js
@@ -3,7 +3,7 @@ const baseUrl = process.env.NEXT_PUBLIC_API_DOMAIN || null
 export const endpoints = {
   api: baseUrl,
   properties: {
-    getData: { url: '/properties', method: 'GET' },
+    getData: { url: `${baseUrl}/properties`, method: 'GET' },
     getProperty: { url: (id) => `${baseUrl}/properties/${id}`, method: 'GET' },
   },
 }
